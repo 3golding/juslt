bf3dce70305a1f6c4a97681f50681b6b2097904b
wx <hnwx7880@163.com>
2018-04-13 15:29:24 +0800

    优化

---
+++ common/src/main/java/com/juslt/common/http/ResponseExt.kt
@@ -0,0 +1,26 @@
+package com.juslt.common.http
+
+import com.google.gson.Gson
+
+/**
+ * Created by phelps on 2018/1/28 0028.
+ */
+inline fun <reified T> Response.parseSuccess(crossinline f:(t: T)->Unit) : Response{
+    success {
+        val from = Gson().fromJson<T>(data, T::class.java)
+        f.invoke(from)
+    }
+    return this
+}
+
+inline fun Response.parseError(crossinline f:(t: ErrorVo)->Unit) : Response{
+    err {
+        f.invoke(ErrorVo(responseCode,data?:""))
+    }
+    return this
+}
+
+fun Response.log() : Response{
+//    debug(toString())
+    return this
+}
