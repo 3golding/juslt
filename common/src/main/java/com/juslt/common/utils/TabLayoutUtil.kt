96da1d080b1811367f83347ac34ae5652c4721a3
hnwx7880 <hnwx7880@163.com>
2018-06-04 18:30:18 +0800

    menu ui

---
+++ common/src/main/java/com/juslt/common/utils/TabLayoutUtil.kt
@@ -0,0 +1,48 @@
+package com.juslt.common.utils
+
+import android.content.res.Resources
+import android.support.design.widget.TabLayout
+import android.util.TypedValue
+import android.widget.LinearLayout
+import java.lang.reflect.Field
+
+/**
+ * Created by Administrator on 2018/6/4.
+ */
+class TabLayoutUtil{
+    companion object {
+        fun setIndicator(tabs: TabLayout, leftDip: Int, rightDip: Int) {
+            val tabLayout = tabs.javaClass
+            var tabStrip: Field? = null
+            try {
+                tabStrip = tabLayout.getDeclaredField("mTabStrip")
+            } catch (e: NoSuchFieldException) {
+                e.printStackTrace()
+            }
+
+            tabStrip!!.isAccessible = true
+            var llTab: LinearLayout? = null
+            try {
+                llTab = tabStrip.get(tabs) as LinearLayout
+            } catch (e: IllegalAccessException) {
+                e.printStackTrace()
+            }
+
+            val left = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, leftDip.toFloat(), Resources.getSystem().getDisplayMetrics())
+            val right = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, rightDip.toFloat(), Resources.getSystem().getDisplayMetrics())
+
+            for (i in 0 until llTab!!.childCount) {
+                val child = llTab.getChildAt(i)
+                child.setPadding(0, 0, 0, 0)
+                val params = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1.toFloat())
+                params.leftMargin = left.toInt()
+                params.rightMargin = right.toInt()
+                child.layoutParams = params
+                child.invalidate()
+            }
+
+
+        }
+
+    }
+}
