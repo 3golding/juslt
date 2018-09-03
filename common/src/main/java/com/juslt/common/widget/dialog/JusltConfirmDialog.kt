c78474c19a6f56de8e99d8e578c64c3d13384d0e
hnwx7880 <hnwx7880@163.com>
2018-09-03 18:10:55 +0800

    发帖添加图片，添加标签

--- common/src/main/java/com/juslt/common/widget/dialog/JusltConfirmDialog.kt
+++ common/src/main/java/com/juslt/common/widget/dialog/JusltConfirmDialog.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.view.Window
 import android.widget.TextView
 import com.juslt.common.R
+import org.jetbrains.anko.find
 
 /**
  * Created by wx on 2018/6/12.
@@ -15,11 +16,13 @@ import com.juslt.common.R
 class JusltConfirmDialog : DialogFragment() {
     var callbacks:Callback ?= null
     companion object {
-        fun getInstance(title: String = "", content: String = ""):JusltConfirmDialog {
+        fun getInstance(title: String = "", content: String = "",confirmString: String="",cancelString:String =""):JusltConfirmDialog {
             val dialog = JusltConfirmDialog()
             val bundle = Bundle()
             bundle.putString("title",title)
             bundle.putString("content",content)
+            bundle.putString("confirm",confirmString)
+            bundle.putString("cancel",cancelString)
             dialog.arguments = bundle
             return dialog
         }
@@ -38,10 +41,22 @@ class JusltConfirmDialog : DialogFragment() {
         val bundle = arguments
         val title = bundle.getString("title")
         val content = bundle.getString("content")
+        val confirmString = bundle.getString("confirm")
+        val cancelString = bundle.getString("cancel")
         view!!.findViewById<TextView>(R.id.tv_title).text = title
         view!!.findViewById<TextView>(R.id.tv_content).text=content
+        if(content.isEmpty()){
+            view!!.findViewById<TextView>(R.id.tv_content).visibility = View.GONE
+        }
+        if(confirmString.isNotEmpty()){
+            view!!.find<TextView>(R.id.tv_confirm).text = confirmString
+        }
+        if(cancelString.isNotEmpty()){
+            view!!.find<TextView>(R.id.tv_cancel).text = cancelString
+        }
         view!!.findViewById<TextView>(R.id.tv_cancel).setOnClickListener {
             dismissAllowingStateLoss()
+            callbacks!!.cancel()
         }
         view!!.findViewById<TextView>(R.id.tv_confirm).setOnClickListener {
             dismissAllowingStateLoss()
@@ -49,7 +64,9 @@ class JusltConfirmDialog : DialogFragment() {
         }
     }
 
+
     interface Callback{
         fun confirm()
+        fun cancel()
     }
 }
